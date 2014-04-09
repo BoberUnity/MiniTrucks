8220745ff00abd03649b8228680080212f909f3c
BoberUnity <borisberez@rambler.ru>
2014-04-10 00:38:01 +0400

    Карта

--- Scripts/ButtonChangePanel.cs
+++ Scripts/ButtonChangePanel.cs
@@ -1,17 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ButtonChangePanel : MonoBehaviour 
 {
   [SerializeField] private UIPanel disabledPanel = null;
   [SerializeField] private UIPanel enabledPanel = null;
+  [SerializeField] private UIButton[] disableButtons = null;
   
   protected virtual void OnPress(bool isPressed)
   {
     if (!isPressed)
     {
-      disabledPanel.enabled = false;
-      enabledPanel.enabled = true;
+      disabledPanel.animation.Play();
+      StartCoroutine(ChangePanel(disabledPanel.animation.clip.length));
+      foreach (var db in disableButtons)
+      {
+        db.isEnabled = false;
+      }
     }
 	}
+
+  private IEnumerator ChangePanel(float time)
+  {
+    yield return new WaitForSeconds(time);
+    disabledPanel.enabled = false;
+    enabledPanel.enabled = true;
+  }
 }
 
---
+++ Scripts/TruckPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Collections;
+
+public class TruckPosition : MonoBehaviour 
+{
+
+  StreamWriter writer;
+  StreamReader reader;
+  [SerializeField] private string filePath = "";
+  [SerializeField] private string setupFileText = "";
+  [SerializeField] private string readFileText = "";
+
+	void Start () 
+  {
+    writer = new StreamWriter(filePath);
+    writer.Write(setupFileText);
+    writer.Close();
+    Read();
+	}
+
+  void Read()
+  {
+    if (File.Exists(filePath))
+    {
+      reader = new StreamReader(filePath);
+      readFileText = reader.ReadToEnd();
+      reader.Close();
+    }
+  }
+}
